e5002f6e63d4a87dfe7450be85b617eea1be8817
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-05-12 10:25:03 -0400

    fix reqlite spawning and killing during testing

--- server/test/index.js
+++ server/test/index.js
@@ -1,4 +1,11 @@
+const spawn = require('child_process').spawn;
 // require babel require hook
 require('babel-core/register');
-// require main tests
-require('./main');
+	
+const reqlite = spawn('reqlite');
+
+reqlite.stderr.on('data', () => {
+	// require main tests
+	require('./main');
+	startTests(reqlite)	;
+});
