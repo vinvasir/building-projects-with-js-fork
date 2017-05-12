e757b73d8c024b980f2bbfe1ead23dc6210e4a2a
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-05-12 10:40:49 -0400

    set up jwt login with a test

--- server/src/auth/login.js
+++ server/src/auth/login.js
@@ -1,9 +1,13 @@
 import passport from 'passport';
+import jwt from 'jsonwebtoken';
+
+import {auth as authConfig} from '../../config';
 
 export default (app) => {
   app.post('/api/login', passport.authenticate('local'), (req, res) => {
   	if (req.user) {
-  		res.send(req.user);
+  		const token = jwt.sign(req.user, authConfig.jwtSecret);
+  		res.send({user: req.user, token});
   	} else {
   		res.status(401).send({error: 'Error logging in1'});
   	}
